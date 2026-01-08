075971d18b2e09c0343c8f9989cebdeaac98bb36
Kritika Arora <kritikaarora2505@gmail.com>
2026-01-08 15:15:45 +0530

    chore: setup basic express server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import pauseRoutes from "./routes/pause.js";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("SatvaRise backend is running 🌱");
 });
+
+app.use("/api/pause", pauseRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => {
